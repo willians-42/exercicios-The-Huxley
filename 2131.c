1a10864c29b019ff8bdb83d3fb2b03822265c5d3
Willians Fernando de Oliveira <161983962+willians-42@users.noreply.github.com>
2024-03-18 08:00:37 -0300

    Update 2131.c

--- 2131.c
+++ 2131.c
@@ -11,26 +11,25 @@ Criado em 09/09/2020 às 00:42
 
 int main() {
 
-int p = 0, e = 0, i = 0, j = 0, mat[100][100], x = 0;
+    int p = 0, e = 0, i = 0, j = 0, mat[100][100], x = 0;
 
-scanf ("%d%d", &p, &e);
+    scanf ("%d%d", &p, &e);
 
-for (i = 0; i < e; i++) {
-    for (j = 0; j < p; j++){
-        scanf ("%d", &mat[j][i]);
-    }
-}
-
-
-for (i = 0; i < p; i++) {
-    for (j = 0; j < e; j++){
-        if (mat[i][j] == 1){
-            x += 1;
+    for (i = 0; i < e; i++) {
+        for (j = 0; j < p; j++){
+            scanf ("%d", &mat[j][i]);
         }
     }
-    printf("Princesa %d: %d voto(s)\n", i + 1, x);
-    x = 0;
 
-}
+    for (i = 0; i < p; i++) {
+        for (j = 0; j < e; j++){
+            if (mat[i][j] == 1){
+                x += 1;
+            }
+        }
+        printf("Princesa %d: %d voto(s)\n", i + 1, x);
+        x = 0;
+
+    }
 	return 0;
 }
